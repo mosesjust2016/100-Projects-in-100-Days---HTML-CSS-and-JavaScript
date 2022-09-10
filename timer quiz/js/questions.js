7d714a69f1a8603b8155708c73999435abfaccfc
Moses Jasi <mjjustme26@gmail.com>
2022-09-10 09:07:04 +0200

    Added Timer Quiz App

---
+++ timer quiz/js/questions.js
@@ -0,0 +1,43 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is a servant of God and of the Lord Jesus?",
+    answer: "James",
+    options: [
+      "James","God","Paul"
+    ]
+  },
+    {
+
+      numb: 2,
+      question: "What is completed by works?",
+    answer: "Labor",
+    options: ["God","Labor","Servants" ]
+  },
+    {
+    numb: 3,
+    question: "What produces steadfastness?",
+    answer: "Testing of faith",
+    options: [
+      "Product","Love","Testing of faith"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Whom does God oppose?",
+    answer: "Proud",
+    options: [
+      "Apostles","Proud","Happy"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who offered His son upon the alter?",
+    answer: "Abraham",
+    options: [
+      "Aaron","Abraham","Lot"
+    ]
+  },
+  
+];
